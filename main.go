34fef6f30a47e4277f1f0827ded8e8106212d3ed
Divan Visagie <visagiedivan@gmail.com>
2017-08-24 20:05:57 +0200

    pingpong test

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 const PORT string = ":1337"
 
+func handlePing(responseWriter http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(responseWriter, "Pong")
+}
+
 func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/", fs)
+	http.HandleFunc("/ping", handlePing)
 
 	log.Println("Listening on port", PORT)
 	http.ListenAndServe(PORT, nil)
