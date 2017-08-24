1207d1896ec667e94127dc3e9d09802baa2f700c
Divan Visagie <visagiedivan@gmail.com>
2017-08-24 20:13:17 +0200

    add picture handler function and vaadin upload refrence

--- main.go
+++ main.go
@@ -12,10 +12,16 @@ func handlePing(responseWriter http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(responseWriter, "Pong")
 }
 
+func handlePictureUpload(responseWriter http.ResponseWriter, request *http.Request) {
+	fmt.Println("I should handle the picture upload now")
+	fmt.Fprintf(responseWriter, "Got it")
+}
+
 func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/", fs)
 	http.HandleFunc("/ping", handlePing)
+	http.HandleFunc("/pictures", handlePictureUpload)
 
 	log.Println("Listening on port", PORT)
 	http.ListenAndServe(PORT, nil)
